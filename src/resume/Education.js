76a2111b5564d5dfe7a6c8fcb009247e7ea3de2b
Caio Peres <caiofperes@hotmail.com>
2023-09-27 18:34:11 -0300

    Updated Education

--- src/resume/Education.js
+++ src/resume/Education.js
@@ -6,8 +6,8 @@ class Education extends React.Component {
     render() {
         return(
             <div>
-                <div className="Edu"><b>Undergraduate Student in Computer Science</b></div>
-                <div className="graduation">Sorocaba, Brazil <br></br> Expected graduation date: September 2023</div>
+                <div className="Edu"><b>Bachelor of Computer Science</b></div>
+                <div className="graduation">Sorocaba, SP <br></br> March 2019 - September 2023</div>
                 <div className="ufscar"><i>Universidade Federal de São Carlos</i></div>
             </div>
         );
