d04d501e5287cc5d42f8317e555d8348eae6ca60
Caio Peres <caiofperes@hotmail.com>
2025-06-27 12:34:27 -0300

    Fixed Samsung Logo and added the patent and paper links

--- src/resume/Resume.js
+++ src/resume/Resume.js
@@ -36,7 +36,7 @@ class Resume extends React.Component {
                             <LeftSection>Experience</LeftSection>
                         </SectionDiv>
 
-                        <SectionDiv marginTop={642}>
+                        <SectionDiv marginTop={638}>
                             <LinhaCinza></LinhaCinza>
                             <LeftSection>Education</LeftSection>
                         </SectionDiv>
@@ -73,10 +73,11 @@ class Resume extends React.Component {
                                         Led the development of new features for the <b>Samsung Health</b> Android application, present on every <b>Samsung Galaxy</b> and with more than <b>1 billion</b> downloads on play store. Developed in <b>Kotlin</b> with <b>Jetpack Compose</b>, and collaborated closely with <b>cross-functional teams</b> to meet high performance and accuracy standards.
                                     </Bullet>
                                     <Bullet ul={false}>
-                                        Fine-tuned and trained a <b>Generative AI</b> model from scratch using <b>Pytorch</b> to reconstruct missing parts of images. The model was trained on <b>GPUs</b> achieving <b>98%</b> similarity with real data, and was optimized for efficient execution on <b>Android</b> devices.<br></br>
+                                        Fine-tuned and trained a <b>Generative AI</b> model from scratch using <b>Pytorch</b> to reconstruct missing parts of images. The model was trained on <b>GPUs</b> achieving <b>98%</b> similarity with real data, and was optimized for efficient execution on <b>Android</b> devices.
+                                        {/*Led the development of an <b>Android system-level security service</b> intended to be on <b>Samsung Galaxy flagships</b>. Developed in <b>Java</b> with performance intensive parts in <b>C++</b>. Automated its complex build system which <b>saved days</b> for new configurations.*/}
                                     </Bullet>
                                     <Bullet ul={false}>
-                                        Led research to <b>explore vulnerabilities</b> in electronic devices using advanced <b>deep learning</b> techniques with <b>PyTorch</b>. Conducted <b>over 100</b> experiments to strengthen both the <b>attack and defense</b> and gain insights into the issue.<br></br>
+                                        Led research to <b>explore vulnerabilities</b> in electronic devices using advanced <b>deep learning</b> techniques with <b>PyTorch</b>. Conducted <b>over 100</b> experiments to strengthen both the <b>attack and defense</b> and gain insights into the issue. <a href="https://dsn2025.github.io/cpaccepted.html">[1]</a>
                                     </Bullet>
                                     <Bullet ul={false}>
                                         Implemented a one-dimensional <b>CNN architecture</b> for signal feature extraction and applied preprocessing techniques including signal drift detection, cropping, filtering, and augmentation through shifting and noise injection, which <b>increased data volume by 200%</b>.
@@ -85,7 +86,7 @@ class Resume extends React.Component {
                                         Employed the <b>One vs One</b> model approach resulting in an <b>18% improvement</b> in attack success rate.
                                     </Bullet>
                                     <Bullet ul={false}>
-                                        Decreased sample preprocessing time by <b>17x</b>, with the implementation of <b>parallel processing</b> techniques in both <b>CPU</b> and <b>GPU</b>. <br></br>
+                                        Decreased sample preprocessing time by <b>17x</b>, with the implementation of <b>parallel processing</b> techniques in both <b>CPU</b> and <b>GPU</b>.
                                     </Bullet>
                                 </Bullet>
 
@@ -102,10 +103,10 @@ class Resume extends React.Component {
                                         Utilized <b>Pandas</b>, <b>Pytorch</b>, <b>Scikit-learn</b>, <b>Numpy</b> and <b>Seaborn</b>. Converted the <b>Python</b> inference pipeline to <b>C++</b> for the final deployment on <b>Android</b>, using <b>ONNX Runtime</b>. 
                                     </Bullet>
                                     <Bullet ul={false}>
-                                        This novel approach achieved an accuracy of <b>99%</b> for authenticating users and resulted in the elaboration of an <b>academic paper</b> and submission of a <b>patent</b>. <br></br>
+                                        This novel approach achieved an accuracy of <b>99%</b> for authenticating users and resulted in the elaboration of an <b>academic paper</b> and submission of a <b>patent</b>. <a href="https://drive.google.com/file/d/124qK9xnE40dKGwbh7EYc-JUdy6nnFT3s/view?usp=sharing">[2]</a>
                                     </Bullet>
                                     <Bullet ul={false}>
-                                        Implemented many more software in <b>Java</b> and <b>C/C++</b> for <b>Windows</b>, <b>Android</b> and <b>embedded devices</b>. <br></br>
+                                        Implemented many more software in <b>Java</b> and <b>C/C++</b> for <b>Windows</b>, <b>Android</b> and <b>embedded devices</b>.
                                     </Bullet>
                                 </Bullet>
 
@@ -181,8 +182,16 @@ class Resume extends React.Component {
                             </Description>
                         
                             <LinhaBranca></LinhaBranca>
-                            <Description fontSize="20px" marginBottom="35px">
-                                TOEFL ITP B2
+                            <Description fontSize="16px" marginBottom="0px">
+                                <div style={{marginBottom: "5px"}}>
+                                    <a href="https://dsn2025.github.io/cpaccepted.html">[1]</a> On the Feasibility of Exploiting the USB Power Line for Side-Channel Attacks against Samsung Smartphone Models
+                                </div>
+                                <div style={{marginBottom: "5px"}}>
+                                    <a href="https://drive.google.com/file/d/124qK9xnE40dKGwbh7EYc-JUdy6nnFT3s/view?usp=sharing">[2]</a> US-20250182529-A1
+                                </div>
+                                <div style={{marginBottom: "0px"}}>
+                                    TOEFL ITP B2
+                                </div>
                             </Description>
 
                         </InternBackground>
